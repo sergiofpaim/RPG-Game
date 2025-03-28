f3149bd6ff1d0a48c6490c01c45c5d626d2e59ca
Sérgio Filho Garcia Paim <sergio.filho@outlook.com>
2025-03-28 20:26:59 -0300

    Interaction adicionado

---
+++ src/main/java/rpg/Interaction.java
@@ -0,0 +1,34 @@
+package rpg;
+
+import java.util.List;
+
+import rpg.interfaces.IThing;
+import rpg.things.Item;
+import rpg.things.Player;
+
+public class Interaction {
+    private IThing target;
+    private String action;
+    private String result;
+    private String message;
+
+    public Interaction(IThing target) {
+        this.target = target;
+    }
+
+    public String startInteraction(Player player) {
+        if (target != null) {
+            if (target instanceof rpg.things.Character) {
+                rpg.things.Character character = (rpg.things.Character) target;
+                message = "Interacting with " + character.getName();
+            } else if (target instanceof Item) {
+                Item item = (Item) target;
+                message = "Interacting with item: " + item.getName();
+            }
+        } else {
+            message = "No target found for interaction.";
+        }
+
+        return message;
+    }
+}
--- src/main/java/rpg/Runner.java
+++ src/main/java/rpg/Runner.java
@@ -1,5 +1,10 @@
 package rpg;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import rpg.interfaces.IThing;
+import rpg.things.Character;
 import rpg.things.Player;
 
 public class Runner {
@@ -19,8 +24,59 @@ public class Runner {
         else
             System.out.println(player.processInput(key));
 
+        checkInteraction();
         displayMap();
         showCommands();
+    }
+
+    private static void checkInteraction() {
+        boolean beginInteraction = false;
+        int interactions = 0;
+        List<IThing> interactiveThings = new ArrayList<>();
+
+        int[][] uShapeOffsets = {
+                { 0, -1 },
+                { 0, 1 },
+                { -1, 0 },
+                { 1, -1 },
+                { 1, 1 }
+        };
+
+        for (rpg.things.Item item : game.getItems()) {
+            for (int[] ofset : uShapeOffsets) {
+                if (item.getPositionX() == player.getPositionX() + ofset[0]
+                        && item.getPositionY() == player.getPositionY() + ofset[0]) {
+                    System.out.println("You found a " + item.getName() + "!");
+                    System.out.println("Press " + interactions + " to interact with the item.");
+                    interactiveThings.add(item);
+                    interactions++;
+                    beginInteraction = true;
+                }
+            }
+        }
+        for (Character npc : game.getCharacters().subList(1, game.getCharacters().size())) {
+            for (int[] ofset : uShapeOffsets) {
+                if (npc.getPositionX() == player.getPositionX() + ofset[0]
+                        && npc.getPositionY() == player.getPositionY() + ofset[0]) {
+                    System.out.println("You are near " + npc.getName() + "!");
+                    System.out.println("Press " + interactions + " to interact with them.");
+                    interactiveThings.add(npc);
+                    interactions++;
+                    beginInteraction = true;
+                }
+            }
+        }
+
+        if (beginInteraction) {
+            System.out.println("Do you want to start an interaction? (y/n)");
+            String choice = RPGGame.scan.nextLine().trim().toLowerCase();
+
+            if (choice.equals("y")) {
+                Interaction interaction = new Interaction(interactiveThings.get(RPGGame.scan.nextInt()));
+
+                System.out.println(interaction.startInteraction(player));
+            }
+        }
     }
 
     private static void showCommands() {
